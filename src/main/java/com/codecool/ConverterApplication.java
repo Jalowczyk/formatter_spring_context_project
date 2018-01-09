c306943c5d63efde901af997d8ca41f0f7d4d97b
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-09 15:31:47 +0100

    Add SimpleCsvConverter object and convert methods' call in main method

--- src/main/java/com/codecool/ConverterApplication.java
+++ src/main/java/com/codecool/ConverterApplication.java
@@ -4,20 +4,23 @@ public class ConverterApplication {
 
     public static void main(String[] args) {
         String message = "No input file defined";
-        Format format = null;
+        Format outputFormat = null;
         String path = null;
+        SimpleCsvConverter scc = new SimpleCsvConverter();
 
         if (args.length == 1) {
             path = args[0];
+            scc.convert(path);
 
         }
         else if (args.length == 2) {
             path = args[1];
             for (Format f : Format.values()) {
                 if (f.getFormatValue().equalsIgnoreCase(args[0])) {
-                    format = f;
+                    outputFormat = f;
                 }
             }
+            scc.convert(path, outputFormat);
         } else {
             System.out.println(message);
         }
