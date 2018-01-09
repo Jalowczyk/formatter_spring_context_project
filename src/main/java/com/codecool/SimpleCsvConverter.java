c40cc1f83b29d84b344196a7d3daeaea646c85c6
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-09 15:26:27 +0100

    Implement convert method in SimpleCsvConverter

---
+++ src/main/java/com/codecool/SimpleCsvConverter.java
@@ -0,0 +1,12 @@
+package com.codecool;
+
+public class SimpleCsvConverter {
+
+    public void convert(String file, ConverterApplication.Format outputFormat) {
+        System.out.println("I convert CSV to output format...");
+    }
+
+    public void convert(String file) {
+        System.out.println("I convert CSV to output format..");
+    }
+}
