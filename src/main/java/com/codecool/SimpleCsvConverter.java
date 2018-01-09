5f3106a96341ec04bdf7104d345f670f6e225cbd
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-09 15:32:13 +0100

    Change prints in convert methods

--- src/main/java/com/codecool/SimpleCsvConverter.java
+++ src/main/java/com/codecool/SimpleCsvConverter.java
@@ -3,10 +3,10 @@ package com.codecool;
 public class SimpleCsvConverter {
 
     public void convert(String file, ConverterApplication.Format outputFormat) {
-        System.out.println("I convert CSV to output format...");
+        System.out.println("I convert CSV to " + outputFormat.getFormatValue() + " format...");
     }
 
     public void convert(String file) {
-        System.out.println("I convert CSV to output format..");
+        System.out.println("I convert CSV to table format..");
     }
 }
