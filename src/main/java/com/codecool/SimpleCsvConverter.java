647fb90f3cce551545c32051a50a0fd0edffe3bf
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-13 15:17:12 +0100

    Set FileReader as attribute in SimpleCsvConverter

--- src/main/java/com/codecool/SimpleCsvConverter.java
+++ src/main/java/com/codecool/SimpleCsvConverter.java
@@ -1,12 +1,21 @@
 package com.codecool;
 
+
+import java.nio.file.Path;
+
 public class SimpleCsvConverter {
 
-    public void convert(String file, ConverterApplication.Format outputFormat) {
+    private FileReader fr;
+
+    public SimpleCsvConverter(FileReader fr) {
+        this.fr = fr;
+    }
+
+    public void convert(Path file, ConverterApplication.Format outputFormat) {
         System.out.println("I convert CSV to " + outputFormat.getFormatValue() + " format...");
     }
 
-    public void convert(String file) {
+    public void convert(Path file) {
         System.out.println("I convert CSV to table format..");
     }
 }
