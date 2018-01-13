42fae1e12d62a433b42850fbc3bb4ba858169f86
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-13 20:02:06 +0100

    Add throwing FileNotFoundException in readData

--- src/main/java/com/codecool/readers/CsvFileReader.java
+++ src/main/java/com/codecool/readers/CsvFileReader.java
@@ -1,14 +1,19 @@
 package com.codecool.readers;
 
+import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class CsvFileReader implements FileReader {
+public class CsvFileReader extends AbstractFileReader {
 
-    public List<Map<String, String>> readData(Path path) {
-        System.out.println("Reading data...");
+    public List<Map<String, String>> readData(Path path) throws FileNotFoundException {
+        if (checkIfFileExist(path)) {
+            System.out.println("YAY IT EXISTS");
+        } else {
+            throw new FileNotFoundException();
+        }
         return new ArrayList<>();
     }
 }
