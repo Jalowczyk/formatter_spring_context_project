3481d66cce6965f8c40141603df1c627690eefdf
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-13 17:12:55 +0100

    Implement printToConsole method in XmlOutputFormatter

---
+++ src/main/java/com/codecool/formatters/XmlOutputFormatter.java
@@ -0,0 +1,11 @@
+package com.codecool.formatters;
+
+import java.util.List;
+import java.util.Map;
+
+public class XmlOutputFormatter implements OutputFormatter{
+
+    public void printToConsole(List<Map<String,String>> data) {
+        System.out.println("Converting...");
+    }
+}
