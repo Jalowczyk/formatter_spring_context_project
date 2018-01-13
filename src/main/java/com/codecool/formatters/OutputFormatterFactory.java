01fe5e8042beffe3f3f58dc0392c509185d095bc
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-13 17:37:32 +0100

    Create OutputFormatterFactory

--- src/main/java/com/codecool/formatters/OutputFormatterFactory.java
+++ src/main/java/com/codecool/formatters/OutputFormatterFactory.java
@@ -1,4 +1,10 @@
 package com.codecool.formatters;
 
+import com.codecool.OutputFormat;
+
 public class OutputFormatterFactory {
+
+    public OutputFormatter createByFormat(OutputFormat of) {
+
+    }
 }
