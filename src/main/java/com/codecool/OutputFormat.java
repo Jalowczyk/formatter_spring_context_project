5ec2eba830edfcc1b1fa03bcfc213e596c9487f5
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-13 17:38:29 +0100

    Rebuild OutputFormat to use reflection

--- src/main/java/com/codecool/OutputFormat.java
+++ src/main/java/com/codecool/OutputFormat.java
@@ -1,12 +1,23 @@
 package com.codecool;
 
+import com.codecool.formatters.JsonOutputFormatter;
+import com.codecool.formatters.OutputFormatter;
+import com.codecool.formatters.TableOutputFormatter;
+import com.codecool.formatters.XmlOutputFormatter;
+
 public enum OutputFormat {
-    JSON("json"), XML("xml"), TABLE("table");
+    JSON("json", JsonOutputFormatter.class), XML("xml", XmlOutputFormatter.class), TABLE("table", TableOutputFormatter.class);
 
     private final String formatValue;
+    private final Class<? extends OutputFormatter> outputFormatterClass;
 
-    OutputFormat(String formatValue) {
+    OutputFormat(String formatValue, Class<? extends OutputFormatter> outputFormatterClass) {
         this.formatValue = formatValue;
+        this.outputFormatterClass = outputFormatterClass;
+    }
+
+    public Class<? extends OutputFormatter> getOutputFormatterClass() {
+        return outputFormatterClass;
     }
 
     public String getFormatValue() {
