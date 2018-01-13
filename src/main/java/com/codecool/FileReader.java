a8b8e21bda1b9ed6289186a429dae1be17796820
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-13 18:45:50 +0100

    Change return type in readData

--- src/main/java/com/codecool/FileReader.java
+++ src/main/java/com/codecool/FileReader.java
@@ -1,8 +1,14 @@
 package com.codecool;
 
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class FileReader {
 
-    public void readData(String file) {
+    public List<Map<String, String>> readData(Path path) {
         System.out.println("Reading data...");
+        return new ArrayList<>();
     }
 }
