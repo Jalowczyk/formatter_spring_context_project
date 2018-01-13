3b527c9f54e1ad4828234d4f991e783b38980521
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-13 17:12:43 +0100

    Implement printToConsole method in JsonOutputFormatter

---
+++ src/main/java/com/codecool/formatters/JsonOutputFormatter.java
@@ -0,0 +1,13 @@
+package com.codecool.formatters;
+
+import com.codecool.formatters.OutputFormatter;
+
+import java.util.List;
+import java.util.Map;
+
+public class JsonOutputFormatter implements OutputFormatter {
+
+    public void printToConsole(List<Map<String,String>> data) {
+        System.out.println("Converting...");
+    }
+}
