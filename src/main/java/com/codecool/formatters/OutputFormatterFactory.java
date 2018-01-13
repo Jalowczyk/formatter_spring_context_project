e7d33043ba714f98e76884045ef54ef76727bb16
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-13 18:20:47 +0100

    Implement createByFormat method

--- src/main/java/com/codecool/formatters/OutputFormatterFactory.java
+++ src/main/java/com/codecool/formatters/OutputFormatterFactory.java
@@ -1,10 +1,17 @@
 package com.codecool.formatters;
 
-import com.codecool.OutputFormat;
-
 public class OutputFormatterFactory {
 
     public OutputFormatter createByFormat(OutputFormat of) {
 
+        OutputFormatter outputFormatter = null;
+
+        try {
+            outputFormatter = of.getOutputFormatterClass().newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        return outputFormatter;
     }
 }
