2f338d6ae75072a8c8abc60d434d306eb0fc9ed3
Jagoda Gniadek <gniadek.jagoda@gmail.com>
2018-01-14 14:08:44 +0100

    Implement readData method in readData method in CsvFileReader class

--- src/main/java/com/codecool/readers/CsvFileReader.java
+++ src/main/java/com/codecool/readers/CsvFileReader.java
@@ -1,19 +1,36 @@
 package com.codecool.readers;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-public class CsvFileReader extends AbstractFileReader {
+public class CsvFileReader implements FileReader {
 
-    public List<Map<String, String>> readData(Path path) throws FileNotFoundException {
-        if (checkIfFileExist(path)) {
-            System.out.println("YAY IT EXISTS");
-        } else {
-            throw new FileNotFoundException();
+    public List<Map<String, String>> readData(Path path) {
+        List<Map<String, String>> dataList = new ArrayList<>();
+        List<String> labels;
+
+        try {
+            Scanner reader = new Scanner(path);
+            if (reader.hasNextLine()) {
+                labels = Arrays.asList(reader.nextLine().split("[,;]"));
+            } else {
+                throw new IOException("Empty CSV file!");
+            }
+
+            while (reader.hasNextLine()) {
+                Map<String, String> map = new LinkedHashMap<>();
+                String[] line = reader.nextLine().split("[,;]");
+
+                for (int i = 0; i < line.length; i++) {
+                    map.put(labels.get(i), line[i]);
+                }
+
+                dataList.add(map);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return new ArrayList<>();
+        return dataList;
     }
 }
